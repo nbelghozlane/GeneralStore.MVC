9f27b250bc4044f8602eb18c1d2834db515e805c
Nadia <nbelghoz@gmail.com>
2021-03-25 19:06:03 -0400

    Added code for product edit and product details

--- GeneralStore.MVC/Controllers/ProductController.cs
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using GeneralStore.MVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -67,6 +69,62 @@ namespace GeneralStore.MVC.Controllers
             _db.Products.Remove(product);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        //Get: Edit
+        //Product/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Product product = _db.Products.Find(id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+        }
+
+        //Post: Edit
+        //Product/Edit/{id}
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(product).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        //Get: Details
+        //Product/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Product product = _db.Products.Find(id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+
         }
 
     }
